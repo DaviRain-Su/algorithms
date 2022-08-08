e799f8f692e8ccc607727844c0490d201ce2f5d6
Davirain <davirain.yin@gmail.com>
2022-08-09 00:02:10 +0800

    add stack struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,18 @@
-/// heap sort algorithm
+/// # Introduction to algorithms thrid edition implement by rust programming
+/// 
+/// ## Now Implement 
+///    - heap sort algorithm
+///        - Max Heap
+///             - Asc sort by Max Heap,`asc_sort_with_max_sift`, `heap_sort_by_max_heap`
+///        - Min Heap
+///             - Dec sort by Min Heap, `dec_sort_with_min_sift`, `heap_sort_by_min_heap`
+///    - stack
+///         - push top element
+///         - pop top element
+///    - queue
+///         - push queue tail element
+///         - pop queue head element
+///
 pub mod heap_sort;
+pub mod stack;
+pub mod queue;
---
+++ src/stack.rs
@@ -0,0 +1,75 @@
+
+#[derive(Debug)]
+pub struct Stack<T> {
+    data: Vec<T>,
+    top: usize,
+}
+
+impl <T: Clone> Stack<T> {
+    /// construct stack
+    pub fn new() -> Stack<T> {
+        Self { data: Vec::new(), top: 0 }
+    }
+
+    /// the stack is empty
+    pub fn is_empty(&self) -> bool {
+        if self.top == 0 {
+            true
+        } else {
+            false
+        }
+    }
+
+    /// push one element to stack
+    pub fn push(&mut self, element: T) {
+        self.top += 1;
+        self.data.push(element);
+    }
+
+    /// pop the top element from stack
+    pub fn pop(&mut self) -> anyhow::Result<T> {
+        if self.is_empty() {
+            return Err(anyhow::anyhow!("underflow"))
+        } else {
+            self.top -= 1;
+            let element = self.data.remove(self.top);
+            return Ok(element.clone())
+        }
+    }
+}
+
+
+#[test]
+fn test_is_empty_success() {
+    let empty_stack = Stack::<i32>::new();
+    assert_eq!(true, empty_stack.is_empty());
+}
+
+#[test]
+fn test_is_empty_faild() {
+    let mut empty_stack = Stack::<i32>::new();
+    empty_stack.push(1);
+    assert_eq!(false, empty_stack.is_empty());
+}
+
+#[test]
+fn test_pop_element_success() {
+    let mut stack = Stack::<i32>::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    let ret = stack.pop().unwrap();
+    assert_eq!(ret, 4);
+    assert_eq!(false, stack.is_empty());
+}
+
+#[test]
+fn test_pop_element_faild() {
+    let mut stack = Stack::<i32>::new();
+    let ret = stack.pop();
+    match ret  {
+        Ok(_) => todo!(),
+        Err(err) => assert_eq!(err.to_string(), "underflow".to_string()),
+    }
+}
